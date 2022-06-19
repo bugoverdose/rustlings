291bec5ff324d4c45d620213bbe299108e4b129b
Jeong Jinwoo <jwjeong96@gmail.com>
2022-06-19 15:57:14 +0900

    feat: intro, variables

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,7 @@
 // variables6.rs
 // Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+const NUMBER:i32 = 3; // Constants types must also always be annotated.
 fn main() {
     println!("Number {}", NUMBER);
 }
