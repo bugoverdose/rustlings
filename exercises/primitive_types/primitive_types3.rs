fd90ccef111583cb22f8e309a298102abedb241d
Jeong Jinwoo <jwjeong96@gmail.com>
2022-06-21 23:08:20 +0900

    feat: primitive types

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,13 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a = ["element"; 1000];
+    // let mut a = String::from("");
+    // for _ in (0..10) {
+    //     a.push_str("1234567890");
+    // }
+    println!("{}", a.len()); // len : String, array 등에 존재하는 메서드. (not tuple)
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
